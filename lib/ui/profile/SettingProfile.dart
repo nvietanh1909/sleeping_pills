8a08a1e8e43e8fb6785b241902e1db699465debb
nvietanh1909 <nvietanh.190904@example.com>
2025-01-05 19:25:06 +0700

    #1 - nvietanh1909 create SettingProfile.dart

---
+++ lib/ui/profile/SettingProfile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SettingProfileScreen extends StatefulWidget {
+  const SettingProfileScreen({super.key});
+
+  @override
+  State<SettingProfileScreen> createState() => _SettingProfileScreenState();
+}
+
+class _SettingProfileScreenState extends State<SettingProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
